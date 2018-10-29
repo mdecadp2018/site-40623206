b1245de7c8b0ced610bd2705d460e2abe27d3c87
40623206 <40623206@gm.nfu.edu.tw>
2018-10-29 23:43:08 +0800

    onshapeweek1lesson

--- flaskapp.py
+++ flaskapp.py
@@ -2106,6 +2106,11 @@ init_mathjax();
 <script src="https://scrum-3.github.io/web/brython/brython.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython_stdlib.js"></script>
 -->
+<style>
+img {
+    border:3px solid blue;
+}
+</style>
 '''
 
 
